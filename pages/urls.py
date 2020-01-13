20aa9736ff97e7b78d68be6f2bd17b8c6861f9eb
Leibelhecht <leibelhecht@gmail.com>
2020-01-12 21:51:06 -0800

    push favicon redirect

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf.urls import url
 
 from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/images/favicon.ico')),
+
 ]
